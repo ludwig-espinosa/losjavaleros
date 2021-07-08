ce07a0b97eda80d7aeb84c8fedc9e08ce0fea46a
Nicovenegas <nicovenegas18@gmail.com>
2021-07-08 19:27:42 -0400

    creacion de 2 modelos

---
+++ Despacho/src/Modelo/RedSocial.java
@@ -0,0 +1,9 @@
+
+package Modelo;
+
+public class RedSocial {
+    private int id;
+    private String nombre;
+    private boolean estado;
+    private String codigo;
+}
---
+++ Despacho/src/Modelo/Usuarios.java
@@ -0,0 +1,71 @@
+
+package Modelo;
+
+import java.util.Date;
+
+public class Usuarios {
+    private int idUsuario;
+    private String nombre;
+    private String rut;
+    private String contraseña;
+    private Date inicio;
+    private Date fin;
+    private boolean estado;
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public Date getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(Date inicio) {
+        this.inicio = inicio;
+    }
+
+    public Date getFin() {
+        return fin;
+    }
+
+    public void setFin(Date fin) {
+        this.fin = fin;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
+}
