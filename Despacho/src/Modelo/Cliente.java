f0b86337feb1f0274774d27804cbc0886cd537c8
Nicovenegas <nicovenegas18@gmail.com>
2021-07-08 19:34:06 -0400

    class Banco

---
+++ Despacho/src/Modelo/Cliente.java
@@ -0,0 +1,13 @@
+package Modelo;
+
+import java.util.Date;
+
+public class Cliente {
+    private int id;
+    private String nombre;
+    private String Rut;
+    private Date nacimiento;
+    private boolean estado;
+
+    
+}
