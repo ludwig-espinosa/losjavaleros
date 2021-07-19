e1680ae0a482e51ce3079b6cd3b50bb51ca1b468
Nicovenegas2 <nicovenegas18@gmail.com>
2021-07-19 18:58:14 -0400

    Create Venta.java

---
+++ Proyecto_DreamGifts/src/Modelo/Venta.java
@@ -0,0 +1,124 @@
+package Modelo;
+
+
+public class Venta {
+    private int id;
+    private int idCliente;
+    private int rrss;
+    private int idPack;
+    private int idBanco;
+    private int idEstadoPago;
+    private int monto;
+    private String direccion;
+    private String receptor;
+    private String contactoReceptor;
+    private boolean estado;
+    private String codigoTransaccion;
+    private int idBoleta;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public int getRrss() {
+        return rrss;
+    }
+
+    public void setRrss(int rrss) {
+        this.rrss = rrss;
+    }
+
+    public int getIdPack() {
+        return idPack;
+    }
+
+    public void setIdPack(int idPack) {
+        this.idPack = idPack;
+    }
+
+    public int getIdBanco() {
+        return idBanco;
+    }
+
+    public void setIdBanco(int idBanco) {
+        this.idBanco = idBanco;
+    }
+
+    public int getIdEstadoPago() {
+        return idEstadoPago;
+    }
+
+    public void setIdEstadoPago(int idEstadoPago) {
+        this.idEstadoPago = idEstadoPago;
+    }
+
+    public int getMonto() {
+        return monto;
+    }
+
+    public void setMonto(int monto) {
+        this.monto = monto;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getReceptor() {
+        return receptor;
+    }
+
+    public void setReceptor(String receptor) {
+        this.receptor = receptor;
+    }
+
+    public String getContactoReceptor() {
+        return contactoReceptor;
+    }
+
+    public void setContactoReceptor(String contactoReceptor) {
+        this.contactoReceptor = contactoReceptor;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public String getCodigoTransaccion() {
+        return codigoTransaccion;
+    }
+
+    public void setCodigoTransaccion(String codigoTransaccion) {
+        this.codigoTransaccion = codigoTransaccion;
+    }
+
+    public int getIdBoleta() {
+        return idBoleta;
+    }
+
+    public void setIdBoleta(int idBoleta) {
+        this.idBoleta = idBoleta;
+    }
+    
+    
+}
