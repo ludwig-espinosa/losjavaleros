a80c3edc7621efee9642bca70fe6aacfdc868926
IgnacioJC <ignaciojc.1982@gmail.com>
2021-07-23 11:30:07 -0400

    código articulo

--- Proyecto_DreamGifts/src/Modelo/Articulo.java
+++ Proyecto_DreamGifts/src/Modelo/Articulo.java
@@ -1,5 +1,116 @@
 package Modelo;
 
+import java.util.Date;
+
 public class Articulo {
+    private String nombre;
+    private int codigo;
+    private int cantidad;
+    private int costo;
+    private String proveedor;
+    private String descripcion;
+    private Date fecha;
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the codigo
+     */
+    public int getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * @param codigo the codigo to set
+     */
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    /**
+     * @return the cantidad
+     */
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    /**
+     * @param cantidad the cantidad to set
+     */
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    /**
+     * @return the costo
+     */
+    public int getCosto() {
+        return costo;
+    }
+
+    /**
+     * @param costo the costo to set
+     */
+    public void setCosto(int costo) {
+        this.costo = costo;
+    }
+
+    /**
+     * @return the proveedor
+     */
+    public String getProveedor() {
+        return proveedor;
+    }
+
+    /**
+     * @param proveedor the proveedor to set
+     */
+    public void setProveedor(String proveedor) {
+        this.proveedor = proveedor;
+    }
+
+    /**
+     * @return the area
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param area the area to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    /**
+     * @return the fecha
+     */
+    public Date getFecha() {
+        return fecha;
+    }
+
+    /**
+     * @param fecha the fecha to set
+     */
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+    
+    
+    
+    
     
 }
