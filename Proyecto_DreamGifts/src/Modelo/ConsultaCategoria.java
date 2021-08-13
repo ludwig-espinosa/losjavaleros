ab700087eab368af45a21bc59a30992441a3424b
IgnacioJC <ignaciojc.1982@gmail.com>
2021-08-13 00:20:44 -0400

    Consulta Categoria

--- Proyecto_DreamGifts/src/Modelo/ConsultaCategoria.java
+++ Proyecto_DreamGifts/src/Modelo/ConsultaCategoria.java
@@ -121,7 +121,7 @@ public class ConsultaCategoria {
             rs = ps.executeQuery();
             rs.next();
             if (!rs.absolute(1)) {
-                System.out.println("articulo id no encontrado");
+                System.out.println("IdD de artículo no encontrado");
               return -1; 
             }else{
                 id = Integer.parseInt(rs.getString(1));
